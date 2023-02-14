c23eff582a3f64a4ea70f7f2ec8d0481bd26704e
224063p <224063p@M419K01>
2023-02-14 10:21:04 +0800

    Add project files.

---
+++ githubPrac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
